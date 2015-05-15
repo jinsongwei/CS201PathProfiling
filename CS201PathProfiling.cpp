b24dd7c82d819d80acb611ff286fc1220d8829d4
jinsongwei <songweijin2012@gmail.com>
2015-05-14 19:53:38 -0700

    intial repository

---
+++ CS201PathProfiling.cpp
@@ -0,0 +1,105 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Type.h"
+ 
+using namespace llvm;
+ 
+namespace {
+  // https://github.com/thomaslee/llvm-demo/blob/master/main.cc
+  static Function* printf_prototype(LLVMContext& ctx, Module *mod) {
+    std::vector<Type*> printf_arg_types;
+    printf_arg_types.push_back(Type::getInt8PtrTy(ctx));
+ 
+    FunctionType* printf_type = FunctionType::get(Type::getInt32Ty(ctx), printf_arg_types, true);
+    Function *func = mod->getFunction("printf");
+    if(!func)
+      func = Function::Create(printf_type, Function::ExternalLinkage, Twine("printf"), mod);
+    func->setCallingConv(CallingConv::C);
+    return func;
+  }
+  
+  struct CS201PathProfiling : public FunctionPass {
+    static char ID;
+    LLVMContext *Context;
+    CS201PathProfiling() : FunctionPass(ID) {}
+    GlobalVariable *bbCounter = NULL;
+    GlobalVariable *BasicBlockPrintfFormatStr = NULL;
+    Function *printf_func = NULL;
+ 
+    //----------------------------------
+    bool doInitialization(Module &M) {
+      errs() << "\n---------Starting CS201PathProfiling---------\n";
+      Context = &M.getContext();
+      bbCounter = new GlobalVariable(M, Type::getInt32Ty(*Context), false, GlobalValue::InternalLinkage, ConstantInt::get(Type::getInt32Ty(*Context), 0), "bbCounter");
+      const char *finalPrintString = "BB Count: %d\n";
+      Constant *format_const = ConstantDataArray::getString(*Context, finalPrintString);
+      BasicBlockPrintfFormatStr = new GlobalVariable(M, llvm::ArrayType::get(llvm::IntegerType::get(*Context, 8), strlen(finalPrintString)+1), true, llvm::GlobalValue::PrivateLinkage, format_const, "BasicBlockPrintfFormatStr");
+      printf_func = printf_prototype(*Context, &M);
+ 
+      errs() << "Module: " << M.getName() << "\n";
+ 
+      return true;
+    }
+ 
+    //----------------------------------
+    bool doFinalization(Module &M) {
+      errs() << "-------Finished CS201PathProfiling----------\n";
+ 
+      return false;
+    }
+    
+    //----------------------------------
+    bool runOnFunction(Function &F) override {
+      errs() << "Function: " << F.getName() << '\n';
+ 
+      for(auto &BB: F) {
+        // Add the footer to Main's BB containing the return 0; statement BEFORE calling runOnBasicBlock
+        if(F.getName().equals("main") && isa<ReturnInst>(BB.getTerminator())) { // major hack?
+          addFinalPrintf(BB, Context, bbCounter, BasicBlockPrintfFormatStr, printf_func);
+        }
+        runOnBasicBlock(BB);
+      }
+ 
+      return true; // since runOnBasicBlock has modified the program
+    }
+ 
+    //----------------------------------
+    bool runOnBasicBlock(BasicBlock &BB) {
+      errs() << "BasicBlock: " << BB.getName() << '\n';
+      IRBuilder<> IRB(BB.getFirstInsertionPt()); // Will insert the generated instructions BEFORE the first BB instruction
+ 
+      Value *loadAddr = IRB.CreateLoad(bbCounter);
+      Value *addAddr = IRB.CreateAdd(ConstantInt::get(Type::getInt32Ty(*Context), 1), loadAddr);
+      IRB.CreateStore(addAddr, bbCounter);
+ 
+      for(auto &I: BB)
+        errs() << I << "\n";
+ 
+      return true;
+    }
+ 
+    //----------------------------------
+    // Rest of this code is needed to: printf("%d\n", bbCounter); to the end of main, just BEFORE the return statement
+    // For this, prepare the SCCGraph, and append to last BB?
+    void addFinalPrintf(BasicBlock& BB, LLVMContext *Context, GlobalVariable *bbCounter, GlobalVariable *var, Function *printf_func) {
+      IRBuilder<> builder(BB.getTerminator()); // Insert BEFORE the final statement
+      std::vector<Constant*> indices;
+      Constant *zero = Constant::getNullValue(IntegerType::getInt32Ty(*Context));
+      indices.push_back(zero);
+      indices.push_back(zero);
+      Constant *var_ref = ConstantExpr::getGetElementPtr(var, indices);
+ 
+      Value *bbc = builder.CreateLoad(bbCounter);
+      CallInst *call = builder.CreateCall2(printf_func, var_ref, bbc);
+      call->setTailCall(false);
+    }
+  };
+}
+ 
+char CS201PathProfiling::ID = 0;
+static RegisterPass<CS201PathProfiling> X("pathProfiling", "CS201PathProfiling Pass", false, false);
+
---
+++ Release+Asserts/CS201PathProfiling.d
@@ -0,0 +1,273 @@
+/home/william/Desktop/workspace/llvm/lib/Transforms/CS201PathProfiling/Release+Asserts/CS201PathProfiling.o \
+  /home/william/Desktop/workspace/llvm/lib/Transforms/CS201PathProfiling/Release+Asserts/CS201PathProfiling.d: \
+  CS201PathProfiling.cpp \
+  /home/william/Desktop/workspace/llvm/include/llvm/Pass.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Compiler.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Config/llvm-config.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/PassSupport.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/InitializePasses.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/PassInfo.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/PassRegistry.h \
+  /home/william/Desktop/workspace/llvm/include/llvm-c/Core.h \
+  /home/william/Desktop/workspace/llvm/include/llvm-c/Support.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/DataTypes.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/DenseMap.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/DenseMapInfo.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/type_traits.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/AlignOf.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/MathExtras.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/SwapByteOrder.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/SmallPtrSet.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/StringMap.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/StringRef.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Allocator.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/SmallVector.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/iterator_range.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Memory.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/CBindingWrapping.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Casting.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/RWMutex.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Threading.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Atomic.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Valgrind.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/PassAnalysisSupport.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Module.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Comdat.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/DataLayout.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/DerivedTypes.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Type.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/APFloat.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/APInt.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/ArrayRef.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/None.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/STLExtras.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/ErrorHandling.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Function.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Argument.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/Twine.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/ilist_node.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Attributes.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/FoldingSet.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Value.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Use.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/PointerIntPair.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/BasicBlock.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/ilist.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Instruction.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/DebugLoc.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/TrackingMDRef.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/MetadataTracking.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/PointerUnion.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/User.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/ADT/iterator.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Instruction.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/SymbolTableListTraits.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/CallingConv.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalObject.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Constant.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalValue.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalAlias.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/OperandTraits.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalVariable.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Metadata.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Metadata.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/CodeGen.h \
+  /home/william/Desktop/workspace/llvm/include/llvm-c/TargetMachine.h \
+  /home/william/Desktop/workspace/llvm/include/llvm-c/Target.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Config/Targets.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/Config/AsmPrinters.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/Config/AsmParsers.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/Config/Disassemblers.def \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/IRBuilder.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/ConstantFolder.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Constants.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/InstrTypes.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Instructions.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/LLVMContext.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/Options.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/CommandLine.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/Operator.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/GetElementPtrTypeIterator.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/IR/ValueHandle.h \
+  /home/william/Desktop/workspace/llvm/include/llvm/Support/raw_ostream.h
+
+/home/william/Desktop/workspace/llvm/include/llvm/Pass.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Compiler.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Config/llvm-config.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/PassSupport.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/InitializePasses.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/PassInfo.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/PassRegistry.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm-c/Core.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm-c/Support.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/DataTypes.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/DenseMap.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/type_traits.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/AlignOf.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/MathExtras.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/SmallPtrSet.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/StringMap.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/StringRef.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Allocator.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/iterator_range.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Memory.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/CBindingWrapping.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Casting.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/RWMutex.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Threading.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Atomic.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Valgrind.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Module.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Comdat.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/DataLayout.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/DerivedTypes.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Type.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/APFloat.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/APInt.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/None.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/STLExtras.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Function.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Argument.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/Twine.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Attributes.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/FoldingSet.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Value.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Use.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/BasicBlock.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/ilist.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Instruction.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/DebugLoc.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/TrackingMDRef.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/MetadataTracking.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/PointerUnion.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/User.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/ADT/iterator.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Instruction.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/CallingConv.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalObject.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Constant.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalValue.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalAlias.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/OperandTraits.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/GlobalVariable.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Metadata.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Metadata.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/CodeGen.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm-c/TargetMachine.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm-c/Target.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Config/Targets.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Config/AsmPrinters.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Config/AsmParsers.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Config/Disassemblers.def:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/IRBuilder.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/ConstantFolder.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Constants.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/InstrTypes.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Instructions.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/LLVMContext.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/Options.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/CommandLine.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/Operator.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/GetElementPtrTypeIterator.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/IR/ValueHandle.h:
+
+/home/william/Desktop/workspace/llvm/include/llvm/Support/raw_ostream.h:
